5cf54d6089e75693cafd9a186954a2dd0bf1522e
Gabriel Paulo Rodrigues <gabrielpaulogpsc@gmail.com>
2024-07-31 20:07:51 -0300

    .

---
+++ Atividades_JavaScript/exercicio1.js
@@ -0,0 +1,15 @@
+console.log("Encontrar o maior número");
+
+let numero1 = 10;
+
+let numero2 = 5;
+
+if (numero1 > numero2){
+    console.log("O maior número é " + numero1)
+}
+    else if (numero2 > numero1){
+        console.log("O maior número é" + numero2)
+    }
+    else {
+        console.log("Os números são iguais")
+    }
