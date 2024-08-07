514d689ed56ce93d6cf44e48368289ab315ff787
Gabriel Paulo Rodrigues <gabrielpaulogpsc@gmail.com>
2024-08-06 22:25:34 -0300

    .

--- Atividades_Js/exercicios.js
+++ Atividades_Js/exercicios.js
@@ -96,4 +96,87 @@ function intervalo(){
     document.getElementById('exercicio6').textContent = numero + resultado
 }
 
+function idade(){
 
+    let ano = parseInt(document.getElementById('ano').value);
+    let ano_nascimento = parseInt(document.getElementById('ano_nascimento').value)
+
+    let verificacao_idade = ano - ano_nascimento
+
+    let resultado
+
+    if(verificacao_idade < 18){
+        resultado = 'Não pode votar'
+    }
+    else{resultado = 'Pode votar'
+
+    }
+
+    document.getElementById('exercicio7').textContent = resultado
+}
+
+function media(){
+
+    let nota1 = parseFloat(document.getElementById('nota1').value)
+    let nota2 = parseFloat(document.getElementById('nota2').value)
+
+    let media = (nota1 + nota2) / 2
+
+    let resultado = '';
+
+    if(media >= 6){
+        resultado = 'O aluno foi aprovado com média: ' + media 
+    }
+    else{
+        resultado = 'O aluno foi reprovado com média: ' + media
+    }
+
+    document.getElementById('exercicio8').textContent = resultado
+
+}
+
+
+function maiorSalario(){
+
+    let hora_professor1 = parseFloat(document.getElementById('hora_professor1').value)
+    let valor_professor1 = parseFloat(document.getElementById('valor_professor1').value)
+
+    let hora_professor2 = parseFloat(document.getElementById('hora_professor2').value)
+    let valor_professor2 = parseFloat(document.getElementById('valor_professor2').value)
+
+    let salario_professor1 = hora_professor1 * valor_professor1
+    let salario_professor2 = hora_professor2 * valor_professor2
+
+    let resultado = '';
+
+    if(salario_professor1 > salario_professor2){
+        resultado = 'O professor 1 recebe mais: ' + salario_professor1.toFixed(2);
+    }
+    else if(salario_professor1 < salario_professor2){
+        resultado = 'O professor 2 recebe mais: ' + salario_professor2.toFixed(2);
+    }
+    else{
+        resultado = 'Os dois tem o mesmo salário: ' + salario_professor1.toFixed(2);
+    }
+
+    document.getElementById('exercicio9').textContent = resultado
+
+}
+
+function imparPar(){
+
+    let numero = parseInt(document.getElementById('numero').value);
+
+    resultado = '';
+
+    if(numero % 2 == 0){
+        resultado = 'O número ' + numero + ' é par'
+    }
+    else{
+        resultado = 'O número ' + numero + ' é impar'
+    }
+
+    document.getElementById('exercicio10').textContent = resultado
+
+
+}
